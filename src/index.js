8b0ec12a7ccffe2aed8f48e5a1b8b5ab53570c93
VVIvan3 <vanya.vorobiev1@mail.ru>
2024-02-14 13:41:56 +0300

    Made form value acquire + closing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import './style.css';
+import processProjectCreation from './projectcreation';
 
 const setUpButtons = (() => {
     const addProjectBtn = document.querySelector('.addproject');
     const creationDialog = document.querySelector('.new-project');
+    const createBtn = document.querySelector('.create');
+
     addProjectBtn.addEventListener('click', () => {
         creationDialog.showModal();
     });
+
+    createBtn.addEventListener('click', () => {
+        processProjectCreation();
+        creationDialog.close();
+    })
 })();
--- src/projectcreation.js
+++ src/projectcreation.js
@@ -1,3 +1,5 @@
-export default function() {
-    //
+export default function processProjectCreation() {
+    const inputField = document.querySelector('#newprj');
+    const inputValue = inputField.value;
+    //ADD
 }
