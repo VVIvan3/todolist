777a11804db947f69a088c89664dfb95423ba449
VVIvan3 <vanya.vorobiev1@mail.ru>
2024-02-14 13:29:37 +0300

    Fixes + dialog element for project creation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './style.css'
+import './style.css';
+
+const setUpButtons = (() => {
+    const addProjectBtn = document.querySelector('.addproject');
+    const creationDialog = document.querySelector('.new-project');
+    addProjectBtn.addEventListener('click', () => {
+        creationDialog.showModal();
+    });
+})();
