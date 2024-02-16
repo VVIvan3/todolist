4bb389ded3c5f1a61432a9f22a3719b586ed3490
VVIvan3 <vanya.vorobiev1@mail.ru>
2024-02-16 12:36:49 +0300

    Minor fixes and basic todo creation

--- src/createtodo.js
+++ src/createtodo.js
@@ -1,3 +1,15 @@
+class Todo {
+    constructor(title, desc) {
+        this.title = title;
+        this.desc = desc;
+        this.due = 'blank for now';
+        this.priority = 'blank for now';
+    }
+}
+
 function createTodo() {
-    //
+    const inputTitle = document.querySelector('#todotitle');
+    const inputDesc = document.querySelector('#tododesc');
+
+    const newTodo = new Todo(inputTitle.value, inputDesc.value);
 }
--- src/formvalidation.js
+++ src/formvalidation.js
@@ -1,5 +1,5 @@
 export default function validate() {
-    const itemsToValidate = document.querySelectorAll('.validate');
+    const itemsToValidate = document.querySelectorAll('.todoinput');
     for (const item of itemsToValidate) {
         if (item.value.length < 1) return false;
     }
